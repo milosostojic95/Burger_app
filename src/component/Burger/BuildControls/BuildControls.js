40386cae730495a0c385958333aaaa9204682b41
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-06-24 19:54:12 +0200

    Added Build control

--- src/component/Burger/BuildControls/BuildControls.js
+++ src/component/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import classes from './BuildControls.modal.css';
+import BuildControl from './BuildControl/BuldControl';
 
 const buildControls = (props) => {
+  <div>
 
+  </div>
 }
 
 export default buildControls;
